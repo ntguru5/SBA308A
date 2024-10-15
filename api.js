95018aacd65087a188a2206b85372b3743b80124
Ken Yeh <yehken@gmail.com>
2024-10-14 22:45:15 -0700

    test POST

--- api.js
+++ api.js
@@ -1,7 +1,20 @@
 const API_URL = 'https://dogapi.dog/api/v2';
+const POST_URL = 'https://dummyjson.com/docs';
 
-export async function getDogFacts() {
+export async function getDogFact() {
     const response = await fetch(`${API_URL}/facts`);
+    const data = await response.json();
+    return data;
+}
+
+export async function createDogFact(fact) {
+    const response = await fetch(`${POST_URL}/posts`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(fact)
+    });
     const data = await response.json();
     return data;
 }
--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-import { getDogFacts } from './api.js';
+import { getDogFact, createDogFact } from './api.js';
 
 const factElement = document.getElementById('fact');
 const getFactButton = document.getElementById('get-fact-button');
+const createFactButton = document.getElementById('create-fact-button');
 
 getFactButton.addEventListener('click', async () => {
     try {
-        const fact = await getDogFacts();
+        const fact = await getDogFact();
         const factBody = fact.data[0].attributes.body;
         factElement.innerText = factBody;
     } catch (error) {
         console.error(error);
     }
 });
+
+createFactButton.addEventListener('click', () => {
+    const fact = {
+        title: 'My New Fact',
+        content: 'This is a new fact about dogs!'
+    };
+
+    fetch('https://dummyjson.com/posts', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        // body: JSON.stringify(fact)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+});
+
+// createFactButton.addEventListener('click', async () => {
+//     try {
+//         const fact = {
+//             attributes: {
+//                 body: 'This is a new fact!'
+//             }
+//         };
+//         const response = await createDogFact(fact);
+//         console.log(response);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// });
